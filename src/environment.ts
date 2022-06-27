07ee93f10de1c5c98a768273f397f4e2016acaf2
4life <povchinnikov@nwave.io>
2022-06-27 11:38:16 +0300

    Run vebvitals on dev

--- src/environment.ts
+++ src/environment.ts
@@ -2,6 +2,7 @@ export default function env() {
   if (process.env.NODE_ENV === 'production') {
     return {
       SERVER_URL: 'https://chat-io-server-4life.herokuapp.com/',
+      PRODUCTION: true,
     };
   }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import env from 'environment';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const { PRODUCTION } = env();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,7 +16,7 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+if (!PRODUCTION) {
+  // eslint-disable-next-line no-console
+  reportWebVitals(console.log);
+}
